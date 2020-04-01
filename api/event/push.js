d53bffe1dcbeacbcbe3f7f8911c369fc978ded86
Craig Gordon <cyghfer@gmail.com>
2020-04-01 19:07:00 -0400

    Replace push.js dynamo logic with pg

--- api/event/push.js
+++ api/event/push.js
@@ -7,17 +7,15 @@ webPush.setVapidDetails(
     'S_3sQfZvYAh7LGuKkXreV_kfcmAkdNFL0nAoI2z1P3w'
 );
 
-const checkAgainstFilter = (category, filter) => {
-    if (filter === undefined) return true;
-
-    const gameTitle = category.slice(0, category.indexOf('_'));
-
-    for (const filterGame of filter.Games.values) {
-        if (gameTitle === filterGame) return true;
-    }
-
-    for (const filterCategory of filter.Categories.values) {
-        if (category === filterCategory) return true;
+const isWhitelisted = (game, category, whitelist) => {
+    for (const item of whitelist) {
+        if (item === true) {
+            return true;
+        } else if (item.includes('|')) {
+            if (item === `${game}|${category}`) return true;
+        } else {
+            if (item === game) return true;
+        }
     }
 
     return false;
@@ -25,7 +23,7 @@ const checkAgainstFilter = (category, filter) => {
 
 module.exports = async (req, res) => {
     console.log('req.body:', req.body);
-    // const { Producer, Game, Category, Message } = req.body;
+    const { Producer, Game, Category, SplitName, Pace, Message, Timestamp } = JSON.parse(req.body);
 
     const pool = new Pool({
         host: process.env.DB_HOST,
@@ -35,21 +33,31 @@ module.exports = async (req, res) => {
         port: process.env.DB_PORT,
     });
 
-    res.status(200).send();
-    return;
-
     try {
-        const allPushSubs = (await dynamoClient.query(getAllPushSubsQueryParams).promise()).Items;
+        const dbRes = await pool.query(
+            `SELECT subscription_.endpoint, subscription_.whitelisted, user_.id
+            FROM subscription_, user_
+            WHERE user_.twitch_name = $1 AND domain = 'Push'`,
+            [Producer]
+        );
+
+        const allPushSubs = dbRes.rows;
 
         const massPushResults = await Promise.all(
             allPushSubs.map(async sub => (
-                checkAgainstFilter(Category, sub.Filter)
-                    ? await webPush.sendNotification(JSON.parse(sub.SRT.slice(6)), Message)
+                isWhitelisted(Game, Category, sub.whitelisted)
+                    ? await webPush.sendNotification(JSON.parse(sub.endpoint), Message)
                     : false
             ))
         );
 
-        res.status(200).send(massPushResults ? true : false);
+        await pool.query(
+            `INSERT INTO event_ (producer_id, game_id, category_id, split_name, pace, message, timestamp)
+            values ($1, ${game.id}, ${category.id}, $2, $3, $4, $5)`,
+            [dbRes.rows[0].id, SplitName, Pace, Message, Timestamp]
+        );
+
+        res.status(200);
     } catch (e) {
         res.status(500).send('Error sending push notifications:', e);
     }
